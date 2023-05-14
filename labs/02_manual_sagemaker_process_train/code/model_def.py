8a9a9687d5c994bb58a71bf5abd921b0e5443a6b
eitansela <eitan.sela@gmail.com>
2023-05-14 16:51:40 +0000

    02_manual_sagemaker_process_train.ipynb with California housing dataset

--- labs/02_manual_sagemaker_process_train/code/model_def.py
+++ labs/02_manual_sagemaker_process_train/code/model_def.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 def get_model():
 
-    inputs = tf.keras.Input(shape=(13,))
-    hidden_1 = tf.keras.layers.Dense(13, activation='tanh')(inputs)
-    hidden_2 = tf.keras.layers.Dense(6, activation='sigmoid')(hidden_1)
+    inputs = tf.keras.Input(shape=(8,))
+    hidden_1 = tf.keras.layers.Dense(8, activation='tanh')(inputs)
+    hidden_2 = tf.keras.layers.Dense(4, activation='sigmoid')(hidden_1)
     outputs = tf.keras.layers.Dense(1)(hidden_2)
     return tf.keras.Model(inputs=inputs, outputs=outputs)
